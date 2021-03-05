f6d0ad474bbfb46e447302fa29ed4029283b50bd
navckin <navolotskaia.anna@gmail.com>
2021-03-06 00:38:07 +0300

    Cahnged style

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 //комментарии добавляю я
 int main()
-{std::string name;
- std::cin>>  name;
-    std::cout << "Hello World! from << name <<std::endl;
+{
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello World! from << name <<std::endl;
 }
+
