fa5b36116e5867de1036e0cf154b85bdb6012695
navckin <72002038+navckin@users.noreply.github.com>
2021-03-07 19:15:25 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-using namespace std;
+using namespace std; // дописываю комменты, комментов у меня здесь в мэйне нет. зато есть по заданию в бранч2
 int main()
 { string name;
  cin>>  name;
